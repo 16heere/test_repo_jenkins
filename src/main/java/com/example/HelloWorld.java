e66c7839e346954eb4a49d12e0e734195fcf0af6
Eshar Heer <ec241019@qmul.ac.uk>
2025-03-13 14:44:45 +0000

    worldWithHelloWorld

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.example;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("🌍");
+        System.out.println("Hello World🌍");
     }
 }
